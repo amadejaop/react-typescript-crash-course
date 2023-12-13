f344b8fddd9b2426d2c040fa5596705157331a03
Amadeja <obsteter.amadeja@gmail.com>
2023-12-13 11:34:17 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface IState {
+  people: {
+    name: string,
+    url: string,
+    age: number,
+    note?: string
+  }[]
+}
+
 function App() {
+
+/*   const [number, setNumber] = useState(5);
+
+  const changeNumber = () => {
+    setNumber(true);
+  } */
+
+  const [people, setPeople] = useState<IState["people"]>([]);
+
+/* people.map(person => {
+  person.note
+}); */
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to my Party</h1>
+
     </div>
   );
 }
